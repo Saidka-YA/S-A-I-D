26ac2dd03392e28b50e8793def32f23ba8877561
NesterovIA_ABs-525 <ivannesterov200514@gmail.com>
2026-06-02 02:16:29 +0700

    alph gen in hill

--- hill.cpp
+++ hill.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 #include <vector>
-#include <stdint.h>
+#include <cstdint>
 
 using namespace std;
+
+// Генерация алфавита для шифра
+vector<char32_t> genalphabet() {
+    vector<char32_t> alphabet;
+    for (char32_t c = U'A'; c <= U'Z'; ++c) alphabet.push_back(c);
+    for (char32_t c = U'a'; c <= U'z'; ++c) alphabet.push_back(c);
+    for (char32_t c = U'А'; c <= U'Я'; ++c) alphabet.push_back(c);
+    for (char32_t c = U'а'; c <= U'я'; ++c) alphabet.push_back(c);
+    for (char32_t c = U'0'; c <= U'9'; ++c) alphabet.push_back(c);
+    const char32_t punct[] = {
+        U' ', U'!', U'@', U'#', U'$', U'%', U'^', U'&', U'*',
+        U'(', U')', U'-', U'_', U'=', U'+', U'\\', U'|', U'/',
+        U'"', U':', U';', U'.', U',', U'?'
+    };
+    for (char32_t c : punct) alphabet.push_back(c);
+
+    return alphabet;
+}
 
 
 
 int main() {
-    vector<uint46_t> text;
+    setlocale(LC_ALL, "Rus");
+    
+    return 0;
 }

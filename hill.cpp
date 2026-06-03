9b8028208987625eb09e6c1e2d83d803c92c1a97
NesterovIA_ABs-525 <ivannesterov200514@gmail.com>
2026-06-03 20:21:34 +0700

    Hill math funcs add

--- hill.cpp
+++ hill.cpp
@@ -22,7 +22,45 @@ vector<char32_t> genalphabet() {
     return alphabet;
 }
 
+// делаем mod отдельно, т.к. могут быть отрицательные числа
+int mod(int a, int m) {
+    return ((a % m) + m) % m;
+}
 
+// Расширенный алгоритм Евклида
+int gcd_euclidext(int a, int b, int& u) {
+    if (b == 0) {
+        u = 1;
+        return a;
+    }
+    int u_prev = 1, u_curr = 0;
+    int r1 = a, r2 = b;
+
+    while (r2 != 0) {
+        int q = r1 / r2;
+        int temp = r2;
+        r2 = r1 - r2 * q;
+        r1 = temp;
+
+        temp = u_curr;
+        u_curr = u_prev - q * u_curr;
+        u_prev = temp;
+    }
+
+    u = u_prev;
+    return r1;
+}
+
+// Обратный элемент
+int modInverse(int a, int m) {
+    int u = 0;
+    int gcd = gcd_euclidext(mod(a, m), m, u); // mod на случай отрицательного a
+    if (gcd != 1) {
+        cerr << "Обратный элемент не существует!\n";
+        return 0;
+    }
+    return mod(u, m);
+}
 
 int main() {
     setlocale(LC_ALL, "Rus");
